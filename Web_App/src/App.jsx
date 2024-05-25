60a471dcef8b970eb326ae136f23d6a30e692c07
khiba-k <khibalpemame@gmail.com>
2024-05-25 16:34:25 +0200

    created app

---
+++ Web_App/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      {/* Container 1 */}
+
+      <div  className="container1" style={{backgroundColor: 'black'}}>
+      <p>Hello World</p>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
