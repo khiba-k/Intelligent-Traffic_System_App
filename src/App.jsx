26b38a49a8d2d2ed25634dc9c0b8710044518a49
khiba-k <khibalpemame@gmail.com>
2024-06-27 10:11:19 +0200

    delete icon

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import './App.css';
 import MyGoogleMap from './components/MyGoogleMap';
 import Tabs from './components/Tabs';
 import IconSaveButton from './components/IconSaveButton';
-import axios from 'axios';
 import trafficSpeedIcon from "./assets/speedometerIcon.png";
+import DeleteData from './components/DeleteData';
+
+
 
 function App() {
   const [currentLocation, setCurrentLocation] = useState('');
   const [destination, setDestination] = useState('');
   const [locationName, setLocationName] = useState('');
-  const [activeTab, setActiveTab] = useState("recents");
+
+  const [activeTab, setActiveTab] = useState("Recent");
   const [isTabColumnVisible, setIsTabColumnVisible] = useState(false);
-  const [routes, setRoutes] = useState([]);
 
   useEffect(() => {
     if (window.innerWidth >= 992) {
@@ -24,6 +26,13 @@ function App() {
   const toggleTabColumn = () => {
     setIsTabColumnVisible(!isTabColumnVisible);
   };
+
+
+  useEffect(() => {
+    if (!currentLocation) {
+      setLocationName(destination);
+    }
+  }, [destination]);
 
   const handleCurrentLocationChange = (e) => {
     const value = e.target.value;
@@ -41,20 +50,25 @@ function App() {
     }
   };
 
-  const handleSaveLocation = async () => {
-    try {
-      const response = await axios.post('http://localhost:5000/recents/', { location: locationName });
-      alert(response.data.message);
-    } catch (error) {
-      console.error('There was an error saving the location!', error);
-    }
+
+  const [isBlackAndWhiteTheme, setIsBlackAndWhiteTheme] = useState(false);
+
+  const handleThemeChange = () => {
+    setIsBlackAndWhiteTheme(!isBlackAndWhiteTheme); // Toggle theme state
   };
+  
+
+  // const [activeTab, setActiveTab] = useState('Recent');
 
   return (
+    <div className={` ${isBlackAndWhiteTheme ? 'black-and-white-theme' : ''}`}>
     <div className="bg full-height">
+      {/* container */}
       <div className="container upper-row p-5">
+        {/* upper row */}
         <div className="row">
           <div className="col pr-0">
+
             <div className="input-group mb-3">
               <button
                 className="btn btn-primary d-lg-none"
@@ -81,59 +95,95 @@ function App() {
                 style={{ width: "191px", marginLeft: "10px" }}
               />
               &nbsp;
-              <button 
-                style={{ border: "none", borderRadius: "100%" }}
-                onClick={handleSaveLocation}
-              >
-                <IconSaveButton />
-              </button>
+              <button style={{ border: "none", borderRadius: "100%" }}><IconSaveButton /></button>
+
             </div>
           </div>
           <div className="col">
-            <h3>{locationName}</h3>
+            <h3>{locationName} </h3>
           </div>
         </div>
       </div>
 
+      {/* lower row */}
       <div className="row lower-row">
+        {/* Tab Column */}
         {(isTabColumnVisible || window.innerWidth >= 992) && (
-          <div className="col-lg-2">
-            <div className="row justify-content-center">
-              <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
-            </div>
-          </div>
+           <div className="col-lg-2 ">
+           <div className="row justify-content-center">
+             <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+           </div>
+           <div className="row justify-content-center" style={{ paddingTop: "12px" }}>
+             <div className="card display-card" style={{ width: "200px" }}>
+               <ul className="list-group list-group-flush">
+                 {activeTab !== "Customize" && (
+                   <li className="list-group-item custom-list-group2 list-group">
+                     Lakeside
+                     <DeleteData className="mr-0"/>
+                   </li>
+                 )}
+                 {activeTab !== "Customize" && (
+                   <li className="list-group-item custom-list-group2 list-group">
+                     BEDCO
+                     <DeleteData style={{marginRight: "0px"}}/>
+                   </li>
+                 )}
+                 {activeTab !== "Customize" && (
+                   <li className="list-group-item custom-list-group2 list-group">
+                     Home
+                     <DeleteData/>
+                   </li>
+                 )}
+                 {activeTab !== "Customize" && (
+                   <li className="list-group-item custom-list-group2 list-group">
+                     Maseru Mall
+                     <DeleteData/>
+                   </li>
+                 )}
+                 {activeTab === "Customize" && (
+                   <>
+                     <li className="list-group-item custom-list-group2">
+                       <h5>Customize</h5>
+                     </li>
+                     <li className="list-group-item custom-list-group2">
+                       <button onClick={handleThemeChange}>
+                         Color Change
+                       </button>
+                     </li>
+                   </>
+                 )}
+               </ul>
+             </div>
+           </div>
+         </div>
         )}
-
+        {/* Map column */}
         <div className="col-lg-8 pt-5">
           <div className="container-sm">
-            <MyGoogleMap 
-              currentLocation={currentLocation} 
-              destination={destination} 
-              setLocationName={setLocationName} 
-              setRoutes={setRoutes}
-            />
-            <div className="pt-5">
+            <MyGoogleMap currentLocation={currentLocation} destination={destination} setLocationName={setLocationName} />
+            {/* <div className="pt-5">
               <div className="card w-100" style={{ width: "980px", border: "none" }}>
                 <ul className="list-group" style={{ backgroundColor: "#D9D9D9" }}>
-                  {routes.map((route, index) => (
-                    <li key={index} className="list-group-item justify-content-center suggest-tab">
-                      {route.summary} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                      <img src={trafficSpeedIcon} alt="Traffic Speed Icon" style={{ width: "20px", height: "20px" }} />
-                      &nbsp;&nbsp;&nbsp;{route.legs[0].duration.text} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                      {route.legs[0].distance.text} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                      {new Date().toLocaleTimeString()}
-                    </li>
-                  ))}
+                  <li className="list-group-item justify-content-center suggest-tab">Main North Rd&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <img src={trafficSpeedIcon} alt="Traffic Speed Icon" style={{ width: "20px", height: "20px" }} />&nbsp;&nbsp;&nbsp;45 km/h
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;8.2 km away &nbsp;&nbsp;&nbsp;(19 min) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 11:39 AM</li>
+                  <li className="list-group-item suggest-tab">Naleli-Sekamaneng Rd &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <img src={trafficSpeedIcon} alt="Traffic Speed Icon" style={{ width: "20px", height: "20px" }} />&nbsp;&nbsp;&nbsp;32 km/h &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    10.6 km away &nbsp;&nbsp;&nbsp;(26 min) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 12:05 AM</li>
+                  <li className="list-group-item suggest-tab">Moshoeshoe Rd &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <img src={trafficSpeedIcon} alt="Traffic Speed Icon" style={{ width: "20px", height: "20px" }} />&nbsp;&nbsp;&nbsp;28 km/h &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    7.3 km away (32 min) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 12:11 AM</li>
                 </ul>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
+        {/* Empty column */}
         <div className="col-lg-2"></div>
       </div>
+    </div>
     </div>
   );
 }
 
 export default App;
-
