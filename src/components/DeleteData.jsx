15722bf04787c1539d47ddd8d331c62e572d70f3
khiba-k <khibalpemame@gmail.com>
2024-06-27 18:48:13 +0200

    DeleteData

--- src/components/DeleteData.jsx
+++ src/components/DeleteData.jsx
@@ -6,7 +6,7 @@ function DeleteData() {
         <>  
             {/* &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp; */}
-            <button style={{ border: "none", Color: "#d9d9d9", backgroundColor: "#d9d9d9", boxShadow: "none"}}><img src={remove} alt="Delete Data" /></button>
+            <button style={{ border: "none", borderRadius: "100%", backgroundColor: "#D9D9D9"}}><img src={remove} alt="Delete Data" /></button>
         </>
     )
     
