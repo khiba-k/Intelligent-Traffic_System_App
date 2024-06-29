dc390503b4d7a6f491c52ca2a80a090f59211f3f
Ntlele <ntleledavid07@gmail.com>
2024-06-29 13:23:44 +0200

    Added css the App.css

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,26 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import '../App.css';
-import MyGoogleMap from './MyGoogleMap';
-import Tabs from './Tabs';
-import IconSaveButton from './IconSaveButton';
-import axios from 'axios';
+import "../App.css";
+import MyGoogleMap from "./MyGoogleMap";
+import Tabs from "./Tabs";
+import IconSaveButton from "./IconSaveButton";
+import axios from "axios";
 import trafficSpeedIcon from "../assets/speedometerIcon.png";
 // import IconTheme from './components/IconTheme';
-import IconLogo from './IconLogo';
-
+import IconLogo from "./IconLogo";
 
 function MainPage() {
-  const [currentLocation, setCurrentLocation] = useState('');
-  const [destination, setDestination] = useState('');
-  const [locationName, setLocationName] = useState('');
+  const [currentLocation, setCurrentLocation] = useState("");
+  const [destination, setDestination] = useState("");
+  const [locationName, setLocationName] = useState("");
   const [activeTab, setActiveTab] = useState("recents");
   const [isTabColumnVisible, setIsTabColumnVisible] = useState(false);
 
+  // Added variables
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // Effect to toggle tab column visibility based on window width
   useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Set initial visibility based on window width
     if (window.innerWidth >= 992) {
       setIsTabColumnVisible(true);
     }
+
+    // Add event listener to update window width state on resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup function to remove event listener when component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   const toggleTabColumn = () => {
@@ -45,30 +61,42 @@ function MainPage() {
 
   const handleSaveLocation = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/recents/', { location: locationName });
+      const response = await axios.post("http://localhost:5000/recents/", {
+        location: locationName,
+      });
       alert(response.data.message);
     } catch (error) {
-      console.error('There was an error saving the location!', error);
+      console.error("There was an error saving the location!", error);
     }
   };
   const [isBlackAndWhiteTheme, setIsBlackAndWhiteTheme] = useState(false);
 
   const handleThemeChange = () => {
-    setIsBlackAndWhiteTheme(!isBlackAndWhiteTheme); 
+    setIsBlackAndWhiteTheme(!isBlackAndWhiteTheme);
   };
 
   return (
-    <div className={` ${isBlackAndWhiteTheme ? 'black-and-white-theme' : ''}`}>
-    <div className="bg full-height">
-      <div className="container upper-row p-5">
-        <div className="row">
-        <div className="col" style={{paddingLeft: "0px"}}>
-        <div style={{paddingLeft: "23px", color: "#2BAF6A", fontWeight: "100px"}}>
-        <IconLogo/><p style={{fontFamily: "sans-serif"}}>TKT Smart Traffic</p>
+    <div className={` ${isBlackAndWhiteTheme ? "black-and-white-theme" : ""}`}>
+      <div className="bg full-height parent-card">
+        <div className="container upper-row p-5 first-child">
+          <div className="row">
+            <div className="col" style={{ paddingLeft: "0px" }}>
+              <div
+                style={{
+                  paddingLeft: "23px",
+                  color: "#2BAF6A",
+                  fontWeight: "100px",
+                }}
+              >
+                <IconLogo />
+                <p style={{ fontFamily: "sans-serif" }}>TKT Smart Traffic</p>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-          <div  className="col" style={{marginRight: "0px"}}>
-            <div className="input-group mb-3" style={{width: "800px"}}>
+        <div className="second-child">
+          <div className="col" style={{ marginRight: "0px" }}>
+            <div className="input-group mb-3" style={{ width: "800px" }}>
               <button
                 className="btn btn-primary d-lg-none"
                 onClick={toggleTabColumn}
@@ -94,7 +122,7 @@ function MainPage() {
                 style={{ width: "191px", marginLeft: "10px" }}
               />
               &nbsp;
-              <button 
+              <button
                 style={{ border: "none", borderRadius: "100%" }}
                 onClick={handleSaveLocation}
               >
@@ -106,46 +134,79 @@ function MainPage() {
             <h3>{locationName}</h3>
           </div>
         </div>
-      </div>
 
-      <div className="row lower-row">
-        {(isTabColumnVisible || window.innerWidth >= 992) && (
-          <div className="col-lg-2">
-            <div className="row justify-content-center">
-              <Tabs activeTab={activeTab} setActiveTab={setActiveTab} handleThemeChange={handleThemeChange} />
+        <div className="row lower-row">
+          {(isTabColumnVisible || window.innerWidth >= 992) && (
+            <div className="col-lg-2">
+              <div className="row justify-content-center">
+                <Tabs
+                  activeTab={activeTab}
+                  setActiveTab={setActiveTab}
+                  handleThemeChange={handleThemeChange}
+                />
+              </div>
             </div>
-          </div>
-        )}
+          )}
 
-        <div className="col-lg-8 pt-5">
-          <div className="container-sm">
-            <MyGoogleMap currentLocation={currentLocation} destination={destination} setLocationName={setLocationName} />
-            <div className="pt-5">
-              <div className="card w-100" style={{ width: "980px", border: "none" }}>
-                <ul className="list-group" style={{ backgroundColor: "white" }}>
-                  <li className="list-group-item justify-content-center suggest-tab">
-                    Main North Rd&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <img src={trafficSpeedIcon} alt="Traffic Speed Icon" style={{ width: "20px", height: "20px" }} />&nbsp;&nbsp;&nbsp;45 km/h
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;8.2 km away &nbsp;&nbsp;&nbsp;(19 min) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 11:39 AM
-                  </li>
-                  <li className="list-group-item suggest-tab">
-                    Naleli-Sekamaneng Rd &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <img src={trafficSpeedIcon} alt="Traffic Speed Icon" style={{ width: "20px", height: "20px" }} />&nbsp;&nbsp;&nbsp;32 km/h &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    10.6 km away &nbsp;&nbsp;&nbsp;(26 min) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 12:05 AM
-                  </li>
-                  <li className="list-group-item suggest-tab">
-                    Moshoeshoe Rd &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <img src={trafficSpeedIcon} alt="Traffic Speed Icon" style={{ width: "20px", height: "20px" }} />&nbsp;&nbsp;&nbsp;28 km/h &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    7.3 km away (32 min) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 12:11 AM
-                  </li>
-                </ul>
+          <div className="col-lg-8 pt-5">
+            <div className="container-sm">
+              <MyGoogleMap
+                currentLocation={currentLocation}
+                destination={destination}
+                setLocationName={setLocationName}
+              />
+              <div className="pt-5">
+                <div
+                  className="card w-100"
+                  style={{ width: "980px", border: "none" }}
+                >
+                  <ul
+                    className="list-group"
+                    style={{ backgroundColor: "white" }}
+                  >
+                    <li className="list-group-item justify-content-center suggest-tab">
+                      Main North Rd&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      <img
+                        src={trafficSpeedIcon}
+                        alt="Traffic Speed Icon"
+                        style={{ width: "20px", height: "20px" }}
+                      />
+                      &nbsp;&nbsp;&nbsp;45 km/h
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;8.2 km away
+                      &nbsp;&nbsp;&nbsp;(19 min)
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 11:39 AM
+                    </li>
+                    <li className="list-group-item suggest-tab">
+                      Naleli-Sekamaneng Rd &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      <img
+                        src={trafficSpeedIcon}
+                        alt="Traffic Speed Icon"
+                        style={{ width: "20px", height: "20px" }}
+                      />
+                      &nbsp;&nbsp;&nbsp;32 km/h
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 10.6 km away
+                      &nbsp;&nbsp;&nbsp;(26 min)
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 12:05 AM
+                    </li>
+                    <li className="list-group-item suggest-tab">
+                      Moshoeshoe Rd &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      <img
+                        src={trafficSpeedIcon}
+                        alt="Traffic Speed Icon"
+                        style={{ width: "20px", height: "20px" }}
+                      />
+                      &nbsp;&nbsp;&nbsp;28 km/h
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 7.3 km away (32 min)
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 12:11 AM
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
+          <div className="col-lg-2"></div>
         </div>
-        <div className="col-lg-2"></div>
       </div>
-    </div>
     </div>
   );
 }
