e60ba5985ef46bb156ba1779b09a1ddab5c31e2c
Ntlele <ntleledavid07@gmail.com>
2024-06-29 19:12:54 +0200

    Made the app responsive

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -77,129 +77,158 @@ function MainPage() {
 
   return (
     <div className={` ${isBlackAndWhiteTheme ? "black-and-white-theme" : ""}`}>
-      <div className="bg full-height parent-card">
-        <div className="container upper-row p-5 first-child">
-          <div className="row">
-            <div className="col" style={{ paddingLeft: "0px" }}>
-              <div
-                style={{
-                  paddingLeft: "23px",
-                  color: "#2BAF6A",
-                  fontWeight: "100px",
-                }}
-              >
-                <IconLogo />
-                <p style={{ fontFamily: "sans-serif" }}>TKT Smart Traffic</p>
+      <div className="bg full-height parent-card border">
+        <div className="first-child">
+          {/* <div className="row"> */}
+          <div className="col" style={{ paddingLeft: "0px" }}>
+            {(isTabColumnVisible || window.innerWidth >= 992) && (
+              <div className="col-lg-2 child-drop-down">
+                <div className="row justify-content-center">
+                  <Tabs
+                    activeTab={activeTab}
+                    setActiveTab={setActiveTab}
+                    handleThemeChange={handleThemeChange}
+                  />
+                </div>
               </div>
-            </div>
+            )}
           </div>
+          {/* </div> */}
         </div>
         <div className="second-child">
-          <div className="col" style={{ marginRight: "0px" }}>
-            <div className="input-group mb-3" style={{ width: "800px" }}>
-              <button
-                className="btn btn-primary d-lg-none"
-                onClick={toggleTabColumn}
-              >
-                {isTabColumnVisible ? "Hide Tabs" : "Show Tabs"}
-              </button>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Current Location"
-                aria-label="Current Location"
-                value={currentLocation}
-                onChange={handleCurrentLocationChange}
-                style={{ width: "191px" }}
-              />
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Destination"
-                aria-label="Destination"
-                value={destination}
-                onChange={handleDestinationChange}
-                style={{ width: "191px", marginLeft: "10px" }}
-              />
-              &nbsp;
-              <button
+          <div
+            className="container upper-row"
+            id="col_container"
+            style={{ marginRight: "0px" }}
+          >
+            <div className="row" id="parent_col">
+              <div className="col col_1 pr-0" id="hide_show">
+                {/* <div
+               style={{
+                paddingLeft: "23px",
+                color: "#2BAF6A",
+                fontWeight: "100px",
+               }}
+               >
+               <IconLogo />
+               <p style={{ fontFamily: "sans-serif" }}>TKT Smart Traffic</p>
+               </div> */}
+                <div className="input-group mb-3" id="input_parent">
+                  <button
+                    className="btn btn-primary d-lg-none"
+                    onClick={toggleTabColumn}
+                  >
+                    {isTabColumnVisible ? "Hide Tabs" : "Show Tabs"}
+                  </button>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Current Location"
+                    aria-label="Current Location"
+                    value={currentLocation}
+                    onChange={handleCurrentLocationChange}
+                    // style={{ width: "191px" }}
+                    id="location_id"
+                  />
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Destination"
+                    aria-label="Destination"
+                    value={destination}
+                    onChange={handleDestinationChange}
+                    // style={{ width: "191px", marginLeft: "10px" }}
+                    id="destination_id"
+                  />
+                  &nbsp;
+                  {/* <button
                 style={{ border: "none", borderRadius: "100%" }}
                 onClick={handleSaveLocation}
-              >
+               >
                 <IconSaveButton />
-              </button>
-            </div>
-          </div>
-          <div className="col">
-            <h3>{locationName}</h3>
-          </div>
-        </div>
-
-        <div className="row lower-row">
-          {(isTabColumnVisible || window.innerWidth >= 992) && (
-            <div className="col-lg-2">
-              <div className="row justify-content-center">
-                <Tabs
-                  activeTab={activeTab}
-                  setActiveTab={setActiveTab}
-                  handleThemeChange={handleThemeChange}
-                />
+               </button> */}
+                </div>
+              </div>
+              <div className="col col_2" id="special_c">
+                <button
+                  style={{
+                    border: "none",
+                    borderRadius: "100%",
+                    background: "white",
+                  }}
+                  onClick={handleSaveLocation}
+                >
+                  <IconSaveButton />
+                </button>
+                <h4
+                  style={{
+                    width: "90%",
+                    marginLeft: "7px",
+                    marginTop: "4px",
+                    overflow: "hidden",
+                    paddingLeft: "20px",
+                  }}
+                >
+                  {locationName}
+                </h4>
               </div>
             </div>
-          )}
-
-          <div className="col-lg-8 pt-5">
-            <div className="container-sm">
-              <MyGoogleMap
-                currentLocation={currentLocation}
-                destination={destination}
-                setLocationName={setLocationName}
-              />
-              <div className="pt-5">
-                <div
-                  className="card w-100"
-                  style={{ width: "980px", border: "none" }}
-                >
-                  <ul
-                    className="list-group"
-                    style={{ backgroundColor: "white" }}
+          </div>
+          <div className="row lower-row">
+            <div className="col-lg-8 pt-5">
+              <div className="container-sm">
+                <MyGoogleMap
+                  currentLocation={currentLocation}
+                  destination={destination}
+                  setLocationName={setLocationName}
+                />
+                <div className="pt-5">
+                  <div
+                    className="card w-100"
+                    style={{ width: "980px", border: "none" }}
                   >
-                    <li className="list-group-item justify-content-center suggest-tab">
-                      Main North Rd&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                      <img
-                        src={trafficSpeedIcon}
-                        alt="Traffic Speed Icon"
-                        style={{ width: "20px", height: "20px" }}
-                      />
-                      &nbsp;&nbsp;&nbsp;45 km/h
-                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;8.2 km away
-                      &nbsp;&nbsp;&nbsp;(19 min)
-                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 11:39 AM
-                    </li>
-                    <li className="list-group-item suggest-tab">
-                      Naleli-Sekamaneng Rd &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                      <img
-                        src={trafficSpeedIcon}
-                        alt="Traffic Speed Icon"
-                        style={{ width: "20px", height: "20px" }}
-                      />
-                      &nbsp;&nbsp;&nbsp;32 km/h
-                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 10.6 km away
-                      &nbsp;&nbsp;&nbsp;(26 min)
-                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 12:05 AM
-                    </li>
-                    <li className="list-group-item suggest-tab">
-                      Moshoeshoe Rd &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                      <img
-                        src={trafficSpeedIcon}
-                        alt="Traffic Speed Icon"
-                        style={{ width: "20px", height: "20px" }}
-                      />
-                      &nbsp;&nbsp;&nbsp;28 km/h
-                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 7.3 km away (32 min)
-                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 12:11 AM
-                    </li>
-                  </ul>
+                    <ul
+                      className="list-group"
+                      style={{ backgroundColor: "gray" }}
+                    >
+                      <li className="list-group-item justify-content-center suggest-tab">
+                        Main North Rd&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img
+                          src={trafficSpeedIcon}
+                          alt="Traffic Speed Icon"
+                          style={{ width: "20px", height: "20px" }}
+                        />
+                        &nbsp;&nbsp;&nbsp;45 km/h
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;8.2 km away
+                        &nbsp;&nbsp;&nbsp;(19 min)
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 11:39 AM
+                      </li>
+                      <li className="list-group-item suggest-tab">
+                        Naleli-Sekamaneng Rd
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img
+                          src={trafficSpeedIcon}
+                          alt="Traffic Speed Icon"
+                          style={{ width: "20px", height: "20px" }}
+                        />
+                        &nbsp;&nbsp;&nbsp;32 km/h
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 10.6 km away
+                        &nbsp;&nbsp;&nbsp;(26 min)
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 12:05 AM
+                      </li>
+                      <li className="list-group-item suggest-tab">
+                        Moshoeshoe Rd &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img
+                          src={trafficSpeedIcon}
+                          alt="Traffic Speed Icon"
+                          style={{ width: "20px", height: "20px" }}
+                        />
+                        &nbsp;&nbsp;&nbsp;28 km/h
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 7.3 km away (32
+                        min) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 12:11 AM
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
